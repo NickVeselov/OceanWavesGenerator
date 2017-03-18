ba8e41e93bdba80131736ee5126ee3e9461b0bca
Nikita Veselov <qamelior@gmail.com>
2017-03-18 16:18:35 +0000

    First working prototype

--- Assets/WavesGenerator.cs
+++ Assets/WavesGenerator.cs
@@ -2,47 +2,104 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WavesGenerator : MonoBehaviour {
+public class WavesGenerator : MonoBehaviour
+{
      Mesh mesh;
-     private Vector3 []vertices;
-     private int sizeX;
-     private int sizeZ;
-     private float t;
-	// Use this for initialization
-	void Start () {
-          sizeX = 100;
-          sizeZ = 100;
-          t = 0;
+
+     private Vector3[] vertices;
+     private Vector3[] baseHeight;
+     private bool[] isEdgePoint;
+     private float zeroWaterLevel;
+
+     public int sizeX;
+     public int sizeZ;
+
+     public int interval_sizeX;
+     public int interval_sizeZ;
+
+     public float scale;
+     public float speed;
+
+     // Use this for initialization
+     void Start()
+     {
           Generate();
+          baseHeight = mesh.vertices;
+          zeroWaterLevel = transform.position.y;
+
+          vertices = new Vector3[baseHeight.Length];
      }
-	
+
      private void Generate()
      {
           mesh = GetComponent<MeshFilter>().mesh;
-          vertices = new Vector3[sizeX * sizeZ];
 
-          for (int i = 0; i!=sizeX; i++)
-               for (int j = 0; j!=sizeZ; j++)
+          vertices = new Vector3[(sizeX + 1) * (sizeZ + 1)];
+          isEdgePoint = new bool[(sizeX + 1) * (sizeZ + 1)];
+
+          //Calculate vertices
+          for (int i = 0, x = -sizeX / 2; x <= sizeX / 2; x++)
+               for (int z = -sizeZ / 2; z <= sizeZ / 2; z++, i++)
                {
-                    vertices[i + j * sizeZ] = new Vector3(i * sizeX, 0, j * sizeZ);
+                    vertices[i] = new Vector3(x * interval_sizeX, 0, z * interval_sizeZ);
+                    if (x == -sizeX / 2 || x == sizeX / 2 || z == -sizeZ / 2 || z == sizeZ / 2)
+                         isEdgePoint[i] = true;
+                    else
+                         isEdgePoint[i] = false;
                }
+
+          //Calculate triangles
+          int[] triangles = new int[sizeX * sizeZ * 6];
+          for (int z = 0, tr_ind = 0, vert_ind = 0;
+               z < sizeZ;
+               z++, vert_ind++)
+               for (int x = 0; x < sizeX;
+                    x++, tr_ind += 6, vert_ind++)
+                    {
+                         triangles[tr_ind + 0] = vert_ind;
+                         triangles[tr_ind + 1] = vert_ind + sizeX + 1;
+                         triangles[tr_ind + 2] = vert_ind + 1;
+                         triangles[tr_ind + 3] = vert_ind + 1;
+                         triangles[tr_ind + 4] = vert_ind + sizeX + 1;
+                         triangles[tr_ind + 5] = vert_ind + sizeX + 2;
+                    }
+
+          Vector2[] uvs = new Vector2[vertices.Length];
+
+          for (int i = 0; i < uvs.Length; i++)
+          {
+               uvs[i] = new Vector2(vertices[i].x, vertices[i].z);
+          }
+
           mesh.vertices = vertices;
+          mesh.triangles = triangles;
+          mesh.RecalculateNormals();
+          mesh.uv = uvs;
      }
 
 
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-     private void FixedUpdate()
+     // Update is called once per frame
+     void Update()
      {
-          for (int i = 0; i != sizeX; i++)
-               for (int j = 0; j != sizeZ; j++)
-               {
-                    vertices[i + j * sizeZ].y += Mathf.Sin(t);
-               }
+          Vector3 vertex;
+          Vector3 globalVertex;
+
+          for (int i = 0; i < vertices.Length; i++)
+          {
+               vertex = baseHeight[i];
+               globalVertex = transform.TransformPoint(vertex);
+
+               if (isEdgePoint[i])
+                    vertex.y = 0;
+               else
+                    vertex.y += scale * -Mathf.Abs(Mathf.Sin(Time.time * speed + new Vector2(globalVertex.x, globalVertex.z).magnitude));
+
+
+               vertices[i] = vertex;
+          }
+
           mesh.vertices = vertices;
-          t += 0.01f;
+          mesh.RecalculateNormals();
+          GetComponent<Renderer>().material.SetTextureOffset("_BumpMap", new Vector2(Time.time * 10, 0));
      }
 }
